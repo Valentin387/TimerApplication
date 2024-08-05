f9ee683bd7fdd2600e54ef48bd1de45051e2b730
Valentin Valencia Valencia <valentinv3v7@gmail.com>
2024-08-04 23:22:11 -0500

    good advance tho

--- app/src/main/java/com/example/timerapplication/MainActivity.kt
+++ app/src/main/java/com/example/timerapplication/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.timerapplication
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,7 +14,9 @@ import com.example.timerapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private lateinit var timer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        navController = findNavController(R.id.nav_view)
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
@@ -31,5 +37,33 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<TextView>(R.id.button)
+
+
+        object: CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                // Update the UI
+                val text01 = "Seconds remaining: "
+                textView.text = text01 + millisUntilFinished / 1000
+            }
+
+            override fun onFinish() {
+                // Update the UI
+                textView.text = "Done!"
+            }
+        }.start()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        timer.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        timer.cancel()
     }
 }
