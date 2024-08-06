7e00febe41083c6bd12f1d403cc747d62d8d8598
Valentin Valencia Valencia <valentinv3v7@gmail.com>
2024-08-05 19:09:08 -0500

    now it works

--- app/src/main/java/com/example/timerapplication/MainActivity.kt
+++ app/src/main/java/com/example/timerapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.CountDownTimer
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,35 +15,18 @@ import com.example.timerapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    private lateinit var binding: ActivityMainBinding
     private lateinit var timer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        navController = findNavController(R.id.nav_view)
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        val textView = findViewById<TextView>(R.id.textView)
-        val button = findViewById<TextView>(R.id.button)
+        setContentView(R.layout.activity_main)
 
 
-        object: CountDownTimer(10000, 1000) {
+        val textView = findViewById<TextView>(R.id.textView01)
+        val button = findViewById<TextView>(R.id.button01)
+
+
+        timer = object: CountDownTimer(20000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 // Update the UI
                 val text01 = "Seconds remaining: "
@@ -53,7 +37,7 @@ class MainActivity : AppCompatActivity() {
                 // Update the UI
                 textView.text = "Done!"
             }
-        }.start()
+        }
 
     }
 
